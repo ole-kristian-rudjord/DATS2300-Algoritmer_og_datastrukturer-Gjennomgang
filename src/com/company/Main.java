81c383cfdaa4fd4411897fac6848f009591d9f0b
ole-kristian-rudjord <s354564@oslomet.no>
2022-06-02 13:49:04 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import com.company.Kapittel_1.Delkapittel_1_1.pk_112;
+
+public class Main {
+    public static void main(String[] args) {
+        // pk 1.1.2
+        int[] heltallsliste = {1,45,78,2,45,87,34,23};
+        System.out.println(pk_112.maks(heltallsliste));
+
+
+    }
+}
